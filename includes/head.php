50814906b8591e8163b8ce7514ef001db2e0698c
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-07-06 13:09:45 -0700

    head y header ok

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Portx - Logistics and Transportation HTML Template</title>
+  <title>Caifornia Express</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- includes/header.php
+++ includes/header.php
@@ -4,75 +4,28 @@
       <button class="close-btn"><i class="fal fa-times"></i></button>
     </div>
     <div class="tpoffcanvas__logo">
-      <a href="index.html">
+      <a href="/">
         <img src="/assets/img/logo/black-logo.png" alt="">
       </a>
     </div>
     <div class="tp-main-menu-mobile"></div>
     <div class="offcanvas__btn mb-20">
-      <a href="contact.html" class="tp-btn w-100">Getting Started</a>
+      <a href="contact.html" class="tp-btn w-100">Contactar ahora</a>
     </div>
     <div class="offcanvas__contact mb-40">
-      <p class="offcanvas__contact-call"><a href="tel:+964-742-44-763">+964 742 44 763</a></p>
-      <p class="offcanvas__contact-mail"><a href="mailto:info@harry.com">info@seomy.com</a></p>
+      <p class="offcanvas__contact-call"><a href="tel:+521234567890">+52 123 456 7890</a></p>
+      <p class="offcanvas__contact-mail"><a href="mailto:info@californiaexpress.com">info@californiaexpress.com</a></p>
     </div>
     <div class="offcanvas__social">
       <a href="#"><i class="fab fa-facebook-f"></i></a>
-      <a href="#"><i class="fab fa-twitter"></i></a>
-      <a href="#"><i class="fab fa-linkedin"></i></a>
+      <!-- <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-linkedin"></i></a> -->
       <a href="#"><i class="fab fa-instagram"></i></a>
     </div>
   </div>
 </div>
 <div class="body-overlay"></div>
-<!--  tp-offcanvus-area end -->
-<!-- search popup start -->
-<div class="search__popup z-index-8">
-  <div class="container">
-    <div class="row">
-      <div class="col-xxl-12">
-        <div class="search__wrapper">
-          <div class="search__top d-flex justify-content-between align-items-center">
-            <div class="search__logo">
-              <a href="index.html">
-                <img src="/assets/img/logo/footer-logo.png" alt="logo">
-              </a>
-            </div>
-            <div class="search__close">
-              <button type="button" class="search__close-btn search-close-btn">
-                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M17 1L1 17" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round" />
-                  <path d="M1 1L17 17" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round" />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div class="search__form">
-            <form action="#">
-              <div class="search__input">
-                <input class="search-input-field" type="text" placeholder="Type here to search...">
-                <span class="search-focus-border"></span>
-                <button type="submit">
-                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M9.55 18.1C14.272 18.1 18.1 14.272 18.1 9.55C18.1 4.82797 14.272 1 9.55 1C4.82797 1 1 4.82797 1 9.55C1 14.272 4.82797 18.1 9.55 18.1Z"
-                      stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M19.0002 19.0002L17.2002 17.2002" stroke="currentColor" stroke-width="1.5"
-                      stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="search-popup-overlay"></div>
-<!-- search popup end -->
+
 <!-- header area start -->
 <header>
   <div class="main-header d-none d-xl-block">
@@ -82,7 +35,7 @@
           <div class="row align-items-center">
             <div class="col-xl-4">
               <div class="main-logo ">
-                <a href="index.html"><img src="/assets/img/logo/black-logo.png" alt=""></a>
+                <a href="/"><img src="/assets/img/logo/black-logo.png" alt=""></a>
               </div>
             </div>
             <div class="col-xl-8">
@@ -92,8 +45,8 @@
                     <div class="tp-header__contact d-flex align-items-center">
                       <span class="tp-header__icon"><i class="flaticon-clock"></i></span>
                       <div class="tp-header__icon-info ml-20">
-                        <label>Sunday Closed</label>
-                        <span>Mon - Sat 9.00 - 18.00</span>
+                        <label>Domingo Cerrado</label>
+                        <span>Lun - Sab 9.00 - 18.00</span>
                       </div>
                     </div>
                   </div>
@@ -102,7 +55,7 @@
                       <span class="tp-header__icon"><i class="flaticon-envelope"></i></span>
                       <div class="tp-header__icon-info ml-20">
                         <label>Email</label>
-                        <span><a href="maito:thepures@support.com">portxinfo@gmail.com</a></span>
+                        <span><a href="maito:info@californiaexpress.com">info@californiaexpress.com</a></span>
                       </div>
                     </div>
                   </div>
@@ -111,7 +64,7 @@
                       <span class="tp-header__icon"><i class="flaticon-telephone"></i></span>
                       <div class="tp-header__icon-info ml-20">
                         <label>Call Us</label>
-                        <span><a href="tel:+98565689656">(00) 122 456 789</a></span>
+                        <span><a href="tel:+521234567890">123 456 7890</a></span>
                       </div>
                     </div>
                   </div>
@@ -130,52 +83,11 @@
               <div class="tp-header__main-menu main-menu">
                 <nav class="tp-main-menu-content">
                   <ul>
-                    <li class="has-dropdown"><a href="index.html">HOME</a>
-                      <div class="tp-submenu submenu has-homemenu">
-                        <div class="row gx-6 row-cols-1 row-cols-md-2 row-cols-xl-3">
-                          <div class="col homemenu">
-                            <div class="homemenu-thumb mb-15">
-                              <img src="/assets/img/menu/home-1.jpg" alt="">
-                              <div class="homemenu-btn">
-                                <a class="tp-menu-btn" href="index.html">View Demo</a>
-                              </div>
-                            </div>
-                            <div class="homemenu-content text-center">
-                              <h4 class="homemenu-title">
-                                <a href="index.html">Home 01</a>
-                              </h4>
-                            </div>
-                          </div>
-                          <div class="col homemenu">
-                            <div class="homemenu-thumb mb-15">
-                              <img src="/assets/img/menu/home-2.jpg" alt="">
-                              <div class="homemenu-btn">
-                                <a class="tp-menu-btn" href="index-2.html">View Demo</a>
-                              </div>
-                            </div>
-                            <div class="homemenu-content text-center">
-                              <h4 class="homemenu-title">
-                                <a href="index-2.html">Home 02</a>
-                              </h4>
-                            </div>
-                          </div>
-                          <div class="col homemenu">
-                            <div class="homemenu-thumb mb-15">
-                              <img src="/assets/img/menu/home-3.jpg" alt="">
-                              <div class="homemenu-btn">
-                                <a class="tp-menu-btn" href="index-3.html">View Demo</a>
-                              </div>
-                            </div>
-                            <div class="homemenu-content text-center">
-                              <h4 class="homemenu-title">
-                                <a href="index-3.html">Home 03</a>
-                              </h4>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
-                    <li class="has-dropdown"><a href="index.html">PAGES</a>
+                    <li class=""><a href="/">Inicio</a></li>
+                    <li class=""><a href="/">Nosotros</a></li>
+                    <li class=""><a href="/">Servicios</a></li>
+                    <li class=""><a href="/">Cobertura</a></li>
+                    <!-- <li class="has-dropdown"><a href="index.html">PAGES</a>
                       <ul class="tp-submenu">
                         <li><a href="about.html">About</a></li>
                         <li><a href="team.html">Team</a></li>
@@ -205,21 +117,15 @@
                         <li><a href="blog-sidebar.html">Blog Sidebar</a></li>
                         <li><a href="blog-details.html">Blog Details</a></li>
                       </ul>
-                    </li>
-                    <li><a href="contact.html">CONTACT</a></li>
+                    </li> -->
+                    <li><a href="/">Contacto</a></li>
                   </ul>
                 </nav>
               </div>
             </div>
             <div class="col-xl-4">
-              <div class="tp-header__right text-end d-flex align-items-center justify-content-end">
-                <div class="search-img f-left mr-30">
-                  <button class="search-open-btn">
-                    <i class="flaticon-loupe"></i>
-                  </button>
-                </div>
                 <div class="tp-header__btn">
-                  <a class="tp-btn" href="contact.html">REQUEST A QUOTE</a>
+                  <a class="tp-btn" href="/">Cotizar</a>
                 </div>
               </div>
             </div>
@@ -233,7 +139,7 @@
       <div class="row align-items-center">
         <div class="col-6">
           <div class="main-logo ">
-            <a href="index.html"><img src="/assets/img/logo/black-logo.png" alt=""></a>
+            <a href="/"><img src="/assets/img/logo/black-logo.png" alt=""></a>
           </div>
         </div>
         <div class="col-6">
